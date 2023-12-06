745b36d903140b7e28804e9bcb8514035df76ad1
KeithGalway <keith.galway@outlook.com>
2023-12-06 15:09:34 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { store } from './app/store'
 import { Provider } from 'react-redux'
 import { disabledReactDevTools } from '@fvilers/disable-react-devtools'
 
-if (process.env.NODE_ENV === 'production') disableReactDevTools()
+if (process.env.NODE_ENV === 'production') disabledReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
